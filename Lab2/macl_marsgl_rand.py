291f15ad35577ac7fd9f6caa5c1b5b215eac57cf
Creator227 <nik.makhnitskiy@gmail.com>
2020-03-27 02:13:19 +0300

    Lab2 Completed

--- Lab2/macl_marsgl_rand.py
+++ Lab2/macl_marsgl_rand.py
@@ -1,42 +1,37 @@
 from randoms import mkm_rand
-import random
-
-
+# Логика следующая: параметры двух ЛКМ генераторов идеальны, тогда T1 = M1, T2 = M2, и если периоды взаимно простые,
+# то период вых. посл максимально большой.
 class mac_mars_rand:
     def __init__(self, k, m1, a1, b1, c1, m2, a2, b2, c2):
-        if m1 < m2:
-            m1, m2 = m2, m1
-        if m1 % m2 == 0:
-            raise ArithmeticError('Invalid Periods! Check initial arguments!')
         self.k = k
         self.__gen_1 = mkm_rand(m1, a1, b1, c1)
-        self.__gen_2 = mkm_rand(m2, a2, b2, c1)
+        self.__gen_2 = mkm_rand(m2, a2, b2, c2)
         self.V = [self.__gen_1.next() for _ in range(self.k)]
 
     def next(self):
-        s = random.randint(0, self.k - 1)
+        s = int(self.__gen_2.next() * self.k)
         res = self.V[s]
-        self.V[s] = self.__gen_2.next()
+        self.V[s] = self.__gen_1.next()
         return res
 
 
 if __name__ == '__main__':
     print('Testing random generators...')
     print('Random generator 3, creating 10000 nums...')
-    generator_3 = mac_mars_rand(256, 64, 35, 9, 11, 123, 123, 124 * 3, 11)
+    generator_3 = mac_mars_rand(256, 64, 35, 9, 11, 135, 234, 16, 19)
     with open('output3.txt', 'w') as fout:
-        for _ in range(1000):
+        for _ in range(10000):
             randint = generator_3.next()
             print(randint, file=fout)
             print('%.5f' % randint, end='\t' if _ % 100 != 0 else '\n')
     print('*********************************************************************************')
     print('Random generator 4, creating 1000 nums...')
-    generator_4 = mac_mars_rand(256, 129, 262147, 2 * 3 * 7 * 13 * 983317 + 1, 17, 123, 123, 124 * 3, 11)
-    with open('output4.txt', 'w') as fout:
-        for _ in range(1000):
-            randint = generator_4.next()
-            print(randint, file=fout)
-            print('%.5f' % randint, end='\t' if _ % 100 != 0 else '\n')
+    # generator_4 = mac_mars_rand(256, 1129208, 262147, 14859, 29, 135, 234, 16, 19)
+    # with open('output4.txt', 'w') as fout:
+    #     for _ in range(1200000):
+    #         randint = generator_4.next()
+    #         print(randint, file=fout)
+    #         print('%.5f' % randint, end='\t' if _ % 100 != 0 else '\n')
 
 
 
--- Lab2/randoms.py
+++ Lab2/randoms.py
@@ -1,6 +1,10 @@
 M1 = 64
-M2 = 2147483648 + 8 * 10089
-# 2147564360 = 2^3 * 3 * 7 * 13 * 983317    b = 2 * 3 * 7 * 13 * 983317 + 1  c = 17
+M2 = 1048576 + 8 * 10079
+#    1048576 + 8 * 10079 = 1 129 208 = 2^3 * 17 * 19^2 * 23;‬
+#    b =  2 * 17 * 19 * 23 + 1 = 14859;
+#    c = 29
+
+# Логика параметров следующая: b-1 кратно любому делителю M, а C взаимно простое с M.
 
 
 class mkm_rand:
@@ -26,9 +30,15 @@ if __name__ == '__main__':
             print('%.5f' % randint, end='\t' if _ % 64 != 0 else '\n')
     print('*********************************************************************************')
     print('Random generator 2, creating 1000 nums...')
-    generator_2 = mkm_rand(M2, 262147, 2 * 3 * 7 * 13 * 983317 + 1, 17)
+    generator_2 = mkm_rand(M2, 262147, 14859, 29)
     with open('output2.txt', 'w') as fout:
-        for _ in range(1000):
+        for _ in range(1200000):
             randint = generator_2.next()
             print(randint, file=fout)
             print('%.5f' % randint, end='\t' if _ % 100 != 0 else '\n')
+    print('Random generator 3, creating 1000 nums...')
+    generator_3 = mkm_rand(135, 234, 16, 19)
+    for _ in range(1200000):
+        randint = generator_2.next()
+        print(randint, file=fout)
+        print('%.5f' % randint, end='\t' if _ % 100 != 0 else '\n')
